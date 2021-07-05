2a4f2ab1bc28ef25e8157bc4fb7179a300e28d24
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-07-04 23:01:45 -0300

    feat(local.guard.ts): removed this unused file

--- src/guards/local/local.guard.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common'
-import { AuthGuard } from '@nestjs/passport'
-
-/**
- * The class that represents the guard that protects some route
- * using the "local" strategy
- */
-@Injectable()
-export class LocalGuard extends AuthGuard('local') {}
