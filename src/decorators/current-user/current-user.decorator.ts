2e9dcd76ca38a5893bf0f309418643ffef298bd5
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-07-04 23:00:21 -0300

    feat(current-user.decorator.ts): renamed this function from "RequestUser" to "CurrentUser"; implemented the "GqlExecutionContext" context creation from the default "ExecutionContext"

---
+++ src/decorators/current-user/current-user.decorator.ts
@@ -0,0 +1,16 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common'
+import { GqlExecutionContext } from '@nestjs/graphql'
+
+import { User } from 'src/modules/user/entities/user.entity'
+
+import { Request } from 'express'
+
+/**
+ * Decorator that is used to get from the request the user data
+ */
+export const CurrentUser = createParamDecorator(
+  (_: unknown, context: ExecutionContext) => {
+    const gqlContext = GqlExecutionContext.create(context)
+    return gqlContext.getContext<{ req: Request & { user: User } }>().req.user
+  },
+)
