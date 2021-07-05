647dce2e28ef30fb30d1b9b623f6a5513805f287
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-07-04 23:10:38 -0300

    feat(auth.resolver.ts): created the "AuthResolver" class; created the "login" method that allows the user perform the login with the application

---
+++ src/modules/auth/resolvers/auth.resolver.ts
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { Args, Mutation, Resolver } from '@nestjs/graphql'
+
+import { AuthService } from '../services/auth.service'
+
+import { LoginInput } from '../models/login.input'
+import { TokenModel } from '../models/token.model'
+
+/**
+ * The class that represents the resolver that deals with the login in the
+ * application
+ */
+@Resolver()
+export class AuthResolver {
+  public constructor(private readonly authService: AuthService) {}
+
+  /**
+   * Method that execute the login
+   *
+   * @param loginInput defines the login input data (email and password)
+   * @returns an object that represents the token data and some more information
+   *
+   * @note The login is being performed this way due to the discussion
+   * found in this issue https://github.com/nestjs/graphql/issues/48
+   */
+  @Mutation(() => TokenModel)
+  public async login(
+    @Args('input', { type: () => LoginInput }) loginInput: LoginInput,
+  ): Promise<TokenModel> {
+    return await this.authService.login(loginInput)
+  }
+}
