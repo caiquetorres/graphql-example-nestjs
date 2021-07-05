bb8c313145272fa23e57315b28864bd51b9b525d
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-07-04 23:15:54 -0300

    fix(app.module.ts): fixed the module dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,15 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { GraphQLService } from './modules/graphql/graphql.service'
 import { TypeOrmService } from './modules/typeorm/typeorm.service'
 
+import { AuthModule } from './modules/auth/auth.module'
 import { EnvModule } from './modules/env/env.module'
+import { PasswordModule } from './modules/password/password.module'
 import { UserModule } from './modules/user/user.module'
 
 @Module({
   imports: [
+    PasswordModule,
+    AuthModule,
     UserModule,
     EnvModule.forRoot({
       envFilePath: ['.env'],
--- src/graphql.ts
+++ src/graphql.ts
@@ -10,21 +10,35 @@
 export interface CreateUserInput {
     email: string;
     name: string;
+    password: string;
+}
+
+export interface LoginInput {
+    email: string;
+    password: string;
 }
 
 export interface IMutation {
     createOneUser(input: CreateUserInput): User | Promise<User>;
+    login(input: LoginInput): TokenModel | Promise<TokenModel>;
 }
 
 export interface IQuery {
     getOneUser(id: string): User | Promise<User>;
 }
 
+export interface TokenModel {
+    expiresIn: string;
+    token: string;
+}
+
 export interface User {
     active?: boolean;
     createdAt?: DateTime;
+    email?: string;
     id: string;
     name?: string;
+    permissions?: string;
     updatedAt?: DateTime;
 }
 
