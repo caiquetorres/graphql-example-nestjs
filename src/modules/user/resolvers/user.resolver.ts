31399f385b36e286c7718fc06b539cd9cb54d05f
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-07-06 00:16:04 -0300

    feat(user.resolver.ts): created the "getMany" method

--- src/modules/user/resolvers/user.resolver.ts
+++ src/modules/user/resolvers/user.resolver.ts
@@ -1,9 +1,34 @@
-import { Resolver } from '@nestjs/graphql'
+import { ConnectionType } from '@nestjs-query/query-graphql'
+import { Args, Query, Resolver } from '@nestjs/graphql'
 
 import { User } from '../entities/user.entity'
+
+import { UserQueryArgs } from '../dtos/user-query.args'
+
+import { UserService } from '../services/user.service'
 
 /**
  * The class that represents the resolver that deals with the user
  */
 @Resolver(() => User)
-export class UserResolver {}
+export class UserResolver {
+  public constructor(private readonly userService: UserService) {}
+
+  /**
+   * Method that searches for user entities based on the sent query
+   *
+   * @param queryArgs defines the how the data will be returned
+   * (paging, filtering and sorting)
+   * @returns all the found elements paginated
+   */
+  @Query(() => UserQueryArgs.ConnectionType)
+  public async getMany(
+    @Args()
+    queryArgs: UserQueryArgs,
+  ): Promise<ConnectionType<User>> {
+    return await UserQueryArgs.ConnectionType.createFromPromise(
+      (query) => this.userService.query(query),
+      queryArgs,
+    )
+  }
+}
