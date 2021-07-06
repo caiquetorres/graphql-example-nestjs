bd7d44264388c01cbfc567efda4ef5818dbb768d
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-07-06 00:16:34 -0300

    docs(graphql.ts & schema.gql): auto-generated files

--- src/graphql.ts
+++ src/graphql.ts
@@ -7,10 +7,72 @@
 
 /* tslint:disable */
 /* eslint-disable */
-export interface CreateUserInput {
-    email: string;
-    name: string;
-    password: string;
+export enum SortDirection {
+    ASC = "ASC",
+    DESC = "DESC"
+}
+
+export enum SortNulls {
+    NULLS_FIRST = "NULLS_FIRST",
+    NULLS_LAST = "NULLS_LAST"
+}
+
+export enum UserSortFields {
+    active = "active",
+    createdAt = "createdAt",
+    email = "email",
+    id = "id",
+    name = "name",
+    updatedAt = "updatedAt"
+}
+
+export interface BooleanFieldComparison {
+    is?: boolean;
+    isNot?: boolean;
+}
+
+export interface CursorPaging {
+    after?: ConnectionCursor;
+    before?: ConnectionCursor;
+    first?: number;
+    last?: number;
+}
+
+export interface DateFieldComparison {
+    between?: DateFieldComparisonBetween;
+    eq?: DateTime;
+    gt?: DateTime;
+    gte?: DateTime;
+    in?: DateTime[];
+    is?: boolean;
+    isNot?: boolean;
+    lt?: DateTime;
+    lte?: DateTime;
+    neq?: DateTime;
+    notBetween?: DateFieldComparisonBetween;
+    notIn?: DateTime[];
+}
+
+export interface DateFieldComparisonBetween {
+    lower: DateTime;
+    upper: DateTime;
+}
+
+export interface IDFilterComparison {
+    eq?: string;
+    gt?: string;
+    gte?: string;
+    iLike?: string;
+    in?: string[];
+    is?: boolean;
+    isNot?: boolean;
+    like?: string;
+    lt?: string;
+    lte?: string;
+    neq?: string;
+    notILike?: string;
+    notIn?: string[];
+    notLike?: string;
 }
 
 export interface LoginInput {
@@ -18,13 +80,53 @@ export interface LoginInput {
     password: string;
 }
 
+export interface StringFieldComparison {
+    eq?: string;
+    gt?: string;
+    gte?: string;
+    iLike?: string;
+    in?: string[];
+    is?: boolean;
+    isNot?: boolean;
+    like?: string;
+    lt?: string;
+    lte?: string;
+    neq?: string;
+    notILike?: string;
+    notIn?: string[];
+    notLike?: string;
+}
+
+export interface UserFilter {
+    active?: BooleanFieldComparison;
+    and?: UserFilter[];
+    createdAt?: DateFieldComparison;
+    email?: StringFieldComparison;
+    id?: IDFilterComparison;
+    name?: StringFieldComparison;
+    or?: UserFilter[];
+    updatedAt?: DateFieldComparison;
+}
+
+export interface UserSort {
+    direction: SortDirection;
+    field: UserSortFields;
+    nulls?: SortNulls;
+}
+
 export interface IMutation {
-    createOneUser(input: CreateUserInput): User | Promise<User>;
     login(input: LoginInput): TokenModel | Promise<TokenModel>;
 }
 
+export interface PageInfo {
+    endCursor?: ConnectionCursor;
+    hasNextPage?: boolean;
+    hasPreviousPage?: boolean;
+    startCursor?: ConnectionCursor;
+}
+
 export interface IQuery {
-    getOneUser(id: string): User | Promise<User>;
+    getMany(filter?: UserFilter, paging?: CursorPaging, sorting?: UserSort[]): UserConnection | Promise<UserConnection>;
 }
 
 export interface TokenModel {
@@ -37,9 +139,20 @@ export interface User {
     createdAt?: DateTime;
     email?: string;
     id: string;
-    name?: string;
+    name: string;
     permissions?: string;
     updatedAt?: DateTime;
 }
 
+export interface UserConnection {
+    edges: UserEdge[];
+    pageInfo: PageInfo;
+}
+
+export interface UserEdge {
+    cursor: ConnectionCursor;
+    node: User;
+}
+
+export type ConnectionCursor = any;
 export type DateTime = any;
