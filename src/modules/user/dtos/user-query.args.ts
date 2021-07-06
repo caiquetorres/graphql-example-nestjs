a040defd1a3e0d39e658b747ff87e60724d8805a
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-07-06 00:12:48 -0300

    feat(create-user.input.ts & update-user.input.ts & user-query.args.ts): renamed some files; created the "UserQueryArgs" class to allow filtering, paging and sorting the user data

---
+++ src/modules/user/dtos/user-query.args.ts
@@ -0,0 +1,10 @@
+import { QueryArgsType } from '@nestjs-query/query-graphql'
+import { ArgsType } from '@nestjs/graphql'
+
+import { User } from '../entities/user.entity'
+
+/**
+ * The class that represents the arguments sent to the api for fetching users
+ */
+@ArgsType()
+export class UserQueryArgs extends QueryArgsType(User) {}
