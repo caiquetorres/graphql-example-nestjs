c8d6431b220e6405ed37f35b414a750a7a1f51e9
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-07-10 20:22:38 -0300

    docs(i18n.filter.ts): added some docs in this class and it methods

--- src/filters/i18n/i18n.filter.ts
+++ src/filters/i18n/i18n.filter.ts
@@ -3,10 +3,20 @@ import { GqlArgumentsHost, GqlExceptionFilter } from '@nestjs/graphql'
 
 import { I18nService } from 'nestjs-i18n'
 
+/**
+ * Class that represents the filter that capture some exception and tranlate
+ * it for some language
+ */
 @Catch(HttpException)
 export class I18nFilter implements GqlExceptionFilter {
   constructor(private readonly i18nService: I18nService) {}
 
+  /**
+   * Method that deals with the thrown exceptions
+   *
+   * @param exception stores the thrown exception
+   * @param host stores the host arguments
+   */
   public async catch(
     exception: HttpException,
     host: ArgumentsHost,
