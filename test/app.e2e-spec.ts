eb2361ae4a555b94e241d1709631cd9959480786
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-08-02 13:23:52 -0300

    test(app.e2e-spec.ts): created a simple e2e test for validating if the app is running correctly

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,25 +1,27 @@
 import { INestApplication } from '@nestjs/common'
-import { Test, TestingModule } from '@nestjs/testing'
 
-import { AppModule } from './../src/app.module'
+import { getApplicationInstance } from './utils/app'
+
 import * as request from 'supertest'
 
-describe('AppController (e2e)', () => {
+describe('App (e2e)', () => {
   let app: INestApplication
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile()
-
-    app = moduleFixture.createNestApplication()
+  beforeAll(async () => {
+    app = await getApplicationInstance()
     await app.init()
   })
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be return an object with status 200 (POST)', async () => {
+    return await request(app.getHttpServer())
+      .post('/graphql')
+      .send({
+        query: 'query{posts{edges{node{id}}}}',
+      })
       .expect(200)
-      .expect('Hello World!')
   })
 })
