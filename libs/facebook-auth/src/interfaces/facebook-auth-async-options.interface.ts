12c9b6fe1746149f413a359ae9cfb4ea96b2dc62
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-09-04 08:45:01 -0300

    docs(facebook-auth/*): added some docs in this lib classes and interfaces

--- libs/facebook-auth/src/interfaces/facebook-auth-async-options.interface.ts
+++ libs/facebook-auth/src/interfaces/facebook-auth-async-options.interface.ts
@@ -3,6 +3,9 @@ import { ModuleMetadata, Type } from '@nestjs/common'
 import { FacebookAuthModuleOptions } from './facebook-auth-module-options.interface'
 import { FacebookAuthOptionsFactory } from './facebook-auth-options-factory.interface'
 
+/**
+ * Interface that represents the optional options used to created this module
+ */
 export interface FacebookAuthAsyncOptions
   extends Pick<ModuleMetadata, 'imports'> {
   useExisting?: Type<FacebookAuthOptionsFactory>
--- libs/facebook-auth/src/interfaces/facebook-auth-options-factory.interface.ts
+++ libs/facebook-auth/src/interfaces/facebook-auth-options-factory.interface.ts
@@ -1,7 +1,11 @@
-import { FacebookAuthModuleOptions } from './facebook-auth-module-options.interface';
+import { FacebookAuthModuleOptions } from './facebook-auth-module-options.interface'
 
+/**
+ * Interface that represents all the methods and properties needed to be implemented
+ * by the factory class used to create this module
+ */
 export interface FacebookAuthOptionsFactory {
   createFacebookAuthOptions():
     | FacebookAuthModuleOptions
-    | Promise<FacebookAuthModuleOptions>;
+    | Promise<FacebookAuthModuleOptions>
 }
